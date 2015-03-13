1d6c122c896257ae389fbc134fe49005c81b2e4a
Mark Reid <mreid@mozilla.com>
2015-03-13 15:36:19 -0300

    Add the DNT header per bug 1140167

--- heka/sandbox/decoders/extract_telemetry_dimensions.lua
+++ heka/sandbox/decoders/extract_telemetry_dimensions.lua
@@ -167,6 +167,7 @@ function process_message()
     msg.Fields.geoCountry = read_message("Fields[geoCountry]") or UNK_GEO
     msg.Timestamp         = read_message("Timestamp")
     msg.Fields.Host       = read_message("Fields[Host]")
+    msg.Fields.DNT        = read_message("Fields[DNT]")
 
     msg.Fields.submissionDate = os.date("%Y%m%d", msg.Timestamp / 1e9)
 
