53b4004a762615f221bf1676356df0e710ec98bb
Mike Trinkala <trink@acm.org>
2015-03-17 10:50:22 -0700

    Firefox active instance refinement
    - https://bugzilla.mozilla.org/show_bug.cgi?id=1131819
    - rename users to instances
    - add docs with the updated message matcher

--- heka/sandbox/filters/firefox_active_instances.lua
+++ heka/sandbox/filters/firefox_active_instances.lua
@@ -2,6 +2,22 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+--[[
+Firefox Active Instances
+
+*Example Heka Configuration*
+
+.. code-block:: ini
+
+    [FirefoxActiveInstances]
+    type = "SandboxFilter"
+    filename = "lua_filters/firefox_active_instances.lua"
+    # todo fix the matcher: remove vendor when appVendor lands
+    message_matcher = "Type == 'telemetry' && Fields[docType] == 'main' && Fields[appName] == 'Firefox' && (Fields[appVendor] == 'Mozilla' || Fields[vendor] == 'Mozilla')"
+    ticker_interval = 60
+    preserve_data = true
+--]]
+
 require "circular_buffer"
 require "cjson"
 require "math"
@@ -14,14 +30,15 @@ local floor = math.floor
 local date = os.date
 
 day_cb  = circular_buffer.new(DAYS, 1, SEC_IN_DAY)
-day_cb:set_header(1, "Active Users")
+day_cb:set_header(1, "Active Instances")
 day_hll = {}
 for i=1,DAYS do
     day_hll[i] = hyperloglog.new()
 end
 current_day = -1
 
-local month_names = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+local month_names = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"}
 local MONTHS = #month_names
 month_hll = {}
 for i=1,MONTHS do
@@ -48,7 +65,7 @@ local function update_day(ts, cid, day)
         clear_days(current_day, current_day + DAYS)
         current_day = current_day + delta
     elseif delta <= -DAYS then
-        return 0 -- ignore data in the past
+        return -- ignore data in the past
     end
     local idx = day % DAYS + 1
     if day_hll[idx]:add(cid) then
@@ -64,7 +81,7 @@ local function clear_months(s, n)
     end
 end
 
-local function update_month(ts, cid, day_changed)
+local function update_month(ts, cid, day_changed, day_advanced)
     local month = current_month
     if current_month == -1 or day_changed then
         local t = date("*t", ts / 1e9)
@@ -72,14 +89,17 @@ local function update_month(ts, cid, day_changed)
         if current_month == -1 then current_month = month end
     end
 
-    local delta = month - current_month
-    if delta > 0 then
-        clear_months(current_month, delta)
-        current_month = month
-    elseif delta < -1 then -- if older than a month roll over the year
-        clear_months(current_month, MONTHS + delta)
-        current_month = month
+    if day_advanced then
+        local delta = month - current_month
+        if delta > 0 then
+            clear_months(current_month, delta)
+            current_month = month
+        elseif delta < 0 then -- roll over the year
+            clear_months(current_month, MONTHS + delta)
+            current_month = month
+        end
     end
+
     month_hll[month]:add(cid)
 end
 
@@ -91,14 +111,15 @@ function process_message()
     if type(cid) == "string" then
         local day = floor(ts / (SEC_IN_DAY * 1e9))
         local day_changed = day ~= current_day
+        local day_advanced = day > current_day
         update_day(ts, cid, day)
-        update_month(ts, cid, day_changed)
+        update_month(ts, cid, day_changed, day_advanced)
     end
     return 0
 end
 
 function timer_event(ns)
-    inject_payload("cbuf", "Firefox Active Daily Users", day_cb)
+    inject_payload("cbuf", "Firefox Active Daily Instances", day_cb)
 
     local json = {}
     local idx = current_month
@@ -109,5 +130,5 @@ function timer_event(ns)
         if idx > MONTHS then idx = 1 end
         json[i] = {[month_names[idx]] = month_hll[idx]:count()}
     end
-    inject_payload("json", "Firefox Active Monthly Users", cjson.encode(json))
+    inject_payload("json", "Firefox Active Monthly Instances", cjson.encode(json))
 end
