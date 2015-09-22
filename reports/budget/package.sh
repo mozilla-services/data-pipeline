fc0ee194109a07ea56dec62e5fa3e2847f634806
Mark Reid <mreid@mozilla.com>
2015-09-22 10:14:53 -0300

    Add email alerting

--- reports/budget/package.sh
+++ reports/budget/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=0.1
+VERSION=0.2
 
-tar czvf budget-report-${VERSION}.tar.gz budget.toml run.sh schema_template.json
+tar czvf budget-report-${VERSION}.tar.gz budget.toml run.sh schema_template.json check_targets.py
--- reports/budget/run.sh
+++ reports/budget/run.sh
@@ -23,7 +23,20 @@ aws s3 sync s3://telemetry-private-analysis/budget-report/data/sandbox_preservat
 sed -r "s/__TARGET__/$TARGET/" schema_template.json > schema.json
 heka/bin/hekad -config budget.toml
 
-# TODO: push json to prod report bucket/path
-# aws s3 cp "$OUTPUT/dashboard/data/PipelineBudget.SubmissionSizesbychannelanddate.json" s3://bucket/path/to/budget.json
+# Push json to prod report bucket/path
+DATA="$OUTPUT/dashboard/data/PipelineBudget.SubmissionSizesbychannelanddate.json"
+aws s3 cp "$DATA" s3://net-mozaws-prod-metrics-data/telemetry-budget-dashboard/budget.json --acl bucket-owner-full-control
 
-# TODO: alert if data for $TARGET exceeds expected volume.
+echo "Fetching budget targets"
+aws s3 cp s3://net-mozaws-prod-us-west-2-pipeline-metadata/telemetry-2/budget_targets.json ./
+
+# Alert if data for $TARGET exceeds expected volume.
+ALERT_FROM=telemetry-alerts@mozilla.com
+ALERT_TO=$ALERT_FROM
+echo "Checking if we've exceeded targets"
+python check_targets.py --day $TARGET \
+                        --targets-file budget_targets.json \
+                        --data-file "$DATA" \
+                        --from-email $ALERT_FROM \
+                        --to-email $ALERT_TO \
+                        --verbose
