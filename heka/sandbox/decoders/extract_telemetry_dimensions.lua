b690edc05a05cb2c7c5960795731cdf9641a29a1
Mark Reid <mreid@mozilla.com>
2015-09-22 15:31:52 -0300

    Use the source name as the field name.
    There's no compelling reason to rename "telemetryEnabled" to
    "isExtended", so we'll make it the same for consistency.

--- heka/sandbox/decoders/extract_telemetry_dimensions.lua
+++ heka/sandbox/decoders/extract_telemetry_dimensions.lua
@@ -125,8 +125,8 @@ local function process_json(msg, json, parsed)
         update_field(msg.Fields, "appBuildId"        , info.appBuildID or UNK_DIM)
         update_field(msg.Fields, "normalizedChannel" , fx.normalize_channel(info.appUpdateChannel))
 
-        -- Old telemetry was always "extended"
-        update_field(msg.Fields, "isExtended"        , true)
+        -- Old telemetry was always "enabled"
+        update_field(msg.Fields, "telemetryEnabled"  , true)
 
         -- Do not want default values for these.
         update_field(msg.Fields, "os"        , info.OS)
@@ -154,7 +154,7 @@ local function process_json(msg, json, parsed)
                    update_field(msg.Fields, "os", parsed.environment.system.os.name)
             end
             if type(parsed.environment.settings) == "table" then
-                update_field(msg.Fields, "isExtended", parsed.environment.settings.telemetryEnabled)
+                update_field(msg.Fields, "telemetryEnabled", parsed.environment.settings.telemetryEnabled)
             end
         end
 
@@ -212,7 +212,7 @@ local function process_json(msg, json, parsed)
         update_field(msg.Fields, "appBuildId"        , abi or UNK_DIM)
         update_field(msg.Fields, "normalizedChannel" , fx.normalize_channel(auc))
 
-        -- The "isExtended" flag does not apply to this type of ping.
+        -- The "telemetryEnabled" flag does not apply to this type of ping.
     end
     update_field(msg.Fields, "sampleId", sample(clientId, 100))
     return nil -- processing was successful
