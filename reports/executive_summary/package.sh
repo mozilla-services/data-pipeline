153c301e0206b57f7512819aac054b2047f114b7
Mark Reid <mreid@mozilla.com>
2015-12-01 09:29:41 -0400

    Print error msg if 'reformat' script is missing.
    Can't just wget it since it lives in a private repo :(

--- reports/executive_summary/package.sh
+++ reports/executive_summary/package.sh
@@ -2,6 +2,10 @@
 
 VERSION=0.11
 
-wget https://raw.githubusercontent.com/mozilla/firefox-executive-dashboard/master/data/reformat_v4.py
+if [ ! -s reformat_v4.py ]; then
+    echo "Missing: reformat_v4.py"
+    echo "Go get it from https://github.com/mozilla/firefox-executive-dashboard/blob/master/data/reformat_v4.py"
+    exit 1
+fi
 
 tar czvf executive-report-v4-${VERSION}.tar.gz run.sh run_executive_report.py reformat_v4.py
