b8c5b2f7a9b98e7d725d30a50300e3d89776945a
Mark Reid <mreid@mozilla.com>
2016-04-21 10:32:11 -0700

    Pick an old cmake version

--- bin/install_dependencies.osx.sh
+++ bin/install_dependencies.osx.sh
@@ -1,4 +1,5 @@
-brew install cmake openssl protobuf postgresql
+# As of 20160421, the latest cmake won't work. Install cmake 3.1 as a workaround.
+brew install openssl protobuf postgresql homebrew/versions/cmake31
 if [ -z "$(which go)" ]; then
-	echo "You'll need to install go. See https://golang.org/dl/"
+	echo "You'll need to install go 1.4.x - see https://golang.org/dl/"
 fi
