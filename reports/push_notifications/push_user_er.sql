c14084de9929ab492c4363543effd03b1a2a62bd
Sunah Suh <ssuh@mozilla.com>
2016-10-18 14:58:30 -0500

    Change 'activity_date' to 'normalized_submission_date' and use HLL for distinct client cardinality

--- reports/push_notifications/push_user_er.sql
+++ reports/push_notifications/push_user_er.sql
@@ -11,7 +11,7 @@ WITH sample AS (
 push_accepters AS (
    SELECT DISTINCT client_id,
                    normalized_submission_date AS eligibility_start,
-                   normalized_submission_date + interval '30' DAY AS eligibility_end
+                   CASE WHEN ((normalized_submission_date + interval '30' DAY) > CURRENT_DATE) THEN CURRENT_DATE ELSE (normalized_submission_date + interval '30' DAY) END AS eligibility_end
    FROM sample
    WHERE push_acceptance > 0
 ),
@@ -26,38 +26,35 @@ combined_push_users AS (
 ),
 all_dau AS (
    SELECT DISTINCT client_id,
-                   normalized_submission_date AS activity_date
+                   normalized_submission_date
    FROM sample
 ),
 push_dau AS (
-  -- Presto can't do RANGE window functions nor can it do count distinct in a window, so the array_agg here and in push_mau is a workaround hack.
-  -- I feel dirty.
   SELECT count(DISTINCT p.client_id) AS dau,
-         array_agg(p.client_id) AS client_ids,
-         activity_date
+         merge(hll_create(p.client_id, 15)) AS hll,
+         normalized_submission_date
   FROM all_dau d
   INNER JOIN combined_push_users p ON d.client_id = p.client_id
-  WHERE activity_date BETWEEN eligibility_start AND eligibility_end
+  WHERE normalized_submission_date BETWEEN eligibility_start AND eligibility_end
   GROUP BY 3
 ),
 push_mau AS (
-  -- I'm sorry.
-  SELECT cardinality(array_distinct(flatten(array_agg(client_ids) over (ORDER BY activity_date ROWS BETWEEN 27 PRECEDING AND 0 FOLLOWING)))) AS mau,
-         activity_date
+  SELECT cardinality(merge(hll) over (ORDER BY normalized_submission_date ROWS BETWEEN 27 PRECEDING AND 0 FOLLOWING)) as mau,
+         normalized_submission_date
    FROM push_dau
 ),
 smoothed_dau AS (
-   SELECT activity_date,
-          avg(dau) OVER (ORDER BY activity_date ROWS BETWEEN 6 PRECEDING AND 0 FOLLOWING) AS smoothed_dau
+   SELECT normalized_submission_date,
+          avg(dau) OVER (ORDER BY normalized_submission_date ROWS BETWEEN 6 PRECEDING AND 0 FOLLOWING) AS smoothed_dau
    FROM push_dau
 )
 SELECT mau * 100 AS mau,
        dau * 100 AS dau,
        smoothed_dau * 100 AS smoothed_dau,
-       m.activity_date,
+       m.normalized_submission_date,
        smoothed_dau/mau AS ER
 FROM push_mau m
-JOIN push_dau d ON m.activity_date=d.activity_date
-JOIN smoothed_dau s ON m.activity_date=s.activity_date
-WHERE m.activity_date > (CURRENT_DATE - interval '6' MONTH + interval '30' DAY)
-ORDER BY m.activity_date
+JOIN push_dau d ON m.normalized_submission_date=d.normalized_submission_date
+JOIN smoothed_dau s ON m.normalized_submission_date=s.normalized_submission_date
+WHERE m.normalized_submission_date > (CURRENT_DATE - interval '6' MONTH + interval '30' DAY)
+ORDER BY m.normalized_submission_date
